6e5608aa0815eb370e13d6b2c7eb095a7f654f72
Mostafa Mahmoud <mmzaghlool52@gmail.com>
2019-06-25 04:17:55 +0200

    commited

--- index.py
+++ index.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Hello, World!'
 
-if __name__ == "__main__":
-    app.run()
+# if __name__ == "__main__":
+#     app.run()
